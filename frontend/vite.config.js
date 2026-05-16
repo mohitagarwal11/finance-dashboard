0dda97b92318aeb428d557051e3317834147bee8
mohitagarwal11 <agarwalmohit1011@gmail.com>
2026-05-16 19:42:44 +0530

    feat: update API base URL configuration and enhance Vite server proxy settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  server: {
-    proxy: { "/api": "http://localhost:8000" },
-  },
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      proxy: {
+        "/api": env.BACKEND_URL,
+      },
+    },
+    plugins: [react(), tailwindcss()],
+  };
 });
